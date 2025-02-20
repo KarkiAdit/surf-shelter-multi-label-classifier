de0f457e8bcf71d504b891766c66e5b6bb04af1b
KarkiAdit <adityakarki728@gmail.com>
2025-02-20 00:26:57 -0600

    update setup to build package with necessary dependencies

--- surf_shelter_multi_label_training_pkg_v0/setup.py
+++ surf_shelter_multi_label_training_pkg_v0/setup.py
@@ -1,11 +1,14 @@
 from setuptools import setup, find_packages
 
+# Read the dependencies from requirements file
+def read_requirements():
+    with open("requirements.txt") as f:
+        return f.read().splitlines()
+
 setup(
     name="surf_shelter_multi_label_training_pkg",
     version="0.1",
     packages=find_packages(),
-    package_data={
-        "multi_label_model_trainer": ["warc.paths.gz"],
-    },
     python_requires=">=3.7",
+    install_requires=read_requirements(),
 )
